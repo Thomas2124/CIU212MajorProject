5fd0c1c21b7f4df28dd2ec77e5fd0a4b19b83aad
Thomas2124 <40778004+Thomas2124@users.noreply.github.com>
2019-06-09 22:25:53 +1000

    started on player
    - left/right moving
    - spawn hitbox on mouse click
    - created add force for jumping
    - still need to fix raycast for ground checking

---
+++ LostEra/Assets/Scripts/Thomas_Scripts/Player.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float health = 100f;
+    public float movingSpeed = 10f;
+    public float jumpPower = 10f;
+    public Rigidbody2D rb;
+    public GameObject attackPoint;
+    public GameObject hitBox;
+    public bool isGrounded = false;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //player movement left and right
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(Vector2.left * movingSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(-Vector2.left * movingSpeed * Time.deltaTime);
+        }
+
+        //player crouch
+        if (Input.GetKey(KeyCode.S))
+        {
+
+        }
+
+        //player jump
+        if (Input.GetKey(KeyCode.W) && isGrounded == true)
+        {
+            rb.AddForce(Vector2.up * jumpPower * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            Instantiate(hitBox, attackPoint.transform.position, Quaternion.identity);
+        }
+
+        //Ground checker
+        RaycastHit2D hitInfo = Physics2D.Raycast(gameObject.transform.position, Vector3.down, 1f);
+
+        if (hitInfo.collider.gameObject.CompareTag("floor"))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+    }
+}
