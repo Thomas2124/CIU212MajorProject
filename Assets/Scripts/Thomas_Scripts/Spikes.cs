6ce92d25eacb316ce9dbe40f0185d33a4e89ec5b
Thomas2124 <40778004+Thomas2124@users.noreply.github.com>
2019-09-27 23:59:06 +1000

    level import, side spikes
    - change import settings of levels
    - improved side spikes (Floors and walls need to be added under/over floating spikes inorder for them to appear correctly within the level. Spikes that are surrounded by objects will need to either be removed or replaced with something else).

--- Assets/Scripts/Thomas_Scripts/Spikes.cs
+++ Assets/Scripts/Thomas_Scripts/Spikes.cs
@@ -5,110 +5,20 @@ using UnityEngine;
 public class Spikes : MonoBehaviour
 {
     public GameObject player;
+    bool up = false;
+    bool down = false;
+    bool left = false;
+    bool right = false;
+    RaycastHit2D hitInfo1;
+    RaycastHit2D hitInfo2;
+    RaycastHit2D hitInfo3;
+    RaycastHit2D hitInfo4;
+
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
-
-        GetComponent<BoxCollider2D>().enabled = false;
-
-        Vector3 myVector = transform.localEulerAngles;
-
-        bool b1 = false;
-        bool b2 = false;
-        bool b3 = false;
-        bool b4 = false;
-
-        RaycastHit2D hitInfo1 = Physics2D.Raycast(transform.position, Vector2.up, 1f);
-        RaycastHit2D hitInfo2 = Physics2D.Raycast(transform.position, Vector2.down, 1f);
-        RaycastHit2D hitInfo3 = Physics2D.Raycast(transform.position, Vector2.left, 1f);
-        RaycastHit2D hitInfo4 = Physics2D.Raycast(transform.position, Vector2.right, 1f);
-
-
-        if (hitInfo1.collider != null)
-        {
-            if (hitInfo1.collider.tag == "Floor")
-            {
-                myVector.z = 180f;
-            }
-            else
-            {
-                b1 = true;
-            }
-        }
-        else
-        {
-            b1 = true;
-        }
-
-        if (hitInfo2.collider != null)
-        {
-            if (hitInfo2.collider.tag == "Floor")
-            {
-                myVector.z = 0f;
-            }
-            else
-            {
-                b2 = true;
-            }
-        }
-        else
-        {
-            b2 = true;
-        }
-
-        if (hitInfo3.collider != null)
-        {
-            if (hitInfo3.collider.tag == "Floor")
-            {
-                myVector.z = 270f;
-            }
-            else
-            {
-                b3 = true;
-            }
-        }
-        else
-        {
-            b3 = true;
-        }
-
-        if (hitInfo4.collider != null)
-        {
-            if (hitInfo4.collider.tag == "Floor")
-            {
-                myVector.z = 90f;
-            }
-            else
-            {
-                b4 = true;
-            }
-        }
-        else
-        {
-            b4 = true;
-        }
-
-        if (b1 == true && b2 == true && b3 == true && b4 == true)
-        {
-            Destroy(gameObject);
-        }
-
-        //----------------------------
-
-        /*if (hitInfo1 == true && hitInfo4 == true || hitInfo2 == true && hitInfo4 == true)
-        {
-            myVector.z = 270f;
-        }
-
-        if (hitInfo1 == true && hitInfo4 == true || hitInfo2 == true && hitInfo4 == true)
-        {
-            myVector.z = 90f;
-        }*/
-
-        GetComponent<BoxCollider2D>().enabled = true;
-
-        transform.rotation = Quaternion.Euler(myVector);
+        //GetComponent<BoxCollider2D>().enabled = true;
     }
 
     // Update is called once per frame
@@ -136,5 +46,129 @@ public class Spikes : MonoBehaviour
         player.gameObject.GetComponent<Player>().enabled = true;
         player.gameObject.GetComponent<Rigidbody2D>().simulated = true;
         player.gameObject.GetComponent<Player>().Dead();
+    }
+
+    void ColliderCheck()
+    {
+        hitInfo1 = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y + 0.45f), Vector2.up, 1f);
+        hitInfo2 = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - 0.45f), Vector2.down, 1f);
+        hitInfo3 = Physics2D.Raycast(new Vector2(transform.position.x - 0.45f, transform.position.y), Vector2.left, 1f);
+        hitInfo4 = Physics2D.Raycast(new Vector2(transform.position.x + 0.45f, transform.position.y), Vector2.right, 1f);
+
+        if (hitInfo1.collider != null)
+        {
+            up = true;
+        }
+
+        if (hitInfo2.collider != null)
+        {
+            down = true;
+        }
+
+        if (hitInfo3.collider != null)
+        {
+            left = true;
+        }
+
+        if (hitInfo4.collider != null)
+        {
+            right = true;
+        }
+    }
+
+    public void GameObjectSpin()
+    {
+        Vector3 myVector = transform.localEulerAngles;
+
+        ColliderCheck();
+
+        if (up == true && down == false && left == false && right == false) //up
+        {
+            myVector.z = 180f;
+        }
+        else if (up == false && down == true && left == false && right == false) //down
+        {
+            myVector.z = 0f;
+        }
+        else if (up == false && down == false && left == true && right == false) //left
+        {
+            myVector.z = 270f;
+        }
+        else if (up == false && down == false && left == false && right == true) //right
+        {
+            myVector.z = 90f;
+        }
+        else if (up == true && down == false && left == true && right == false) //up left
+        {
+            if (hitInfo1.collider.tag == "Floor")
+            {
+                myVector.z = 180f;
+            }
+            else
+            {
+                myVector.z = 270f;
+            }
+        }
+        else if (up == true && down == false && left == false && right == true) //up right
+        {
+            if (hitInfo1.collider.tag == "Floor")
+            {
+                myVector.z = 180f;
+            }
+            else
+            {
+                myVector.z = 90f;
+            }
+        }
+        else if (up == false && down == true && left == true && right == false) //down left
+        {
+            if (hitInfo2.collider.tag == "Floor")
+            {
+                myVector.z = 0f;
+            }
+            else
+            {
+                myVector.z = 270f;
+            }
+        }
+        else if (up == false && down == true && left == false && right == true) //down right
+        {
+            if (hitInfo2.collider.tag == "Floor")
+            {
+                myVector.z = 0f;
+            }
+            else
+            {
+                myVector.z = 90f;
+            }
+        }
+        else if (up == true && down == true && left == true && right == false) //up down left
+        {
+            myVector.z = 270f;
+        }
+        else if (up == true && down == true && left == false && right == true) //up down right
+        {
+            myVector.z = 90f;
+        }
+        else if (up == true && down == false && left == true && right == true) //left right up
+        {
+            myVector.z = 180f;
+        }
+        else if (up == false && down == true && left == true && right == true) //left right down
+        {
+            myVector.z = 0f;
+        }
+        else if (up == true && down == true && left == true && right == true) //surrounded
+        {
+            //Destroy(gameObject);
+        }
+        else if (up == false && down == false && left == false && right == false) //nothing around it
+        {
+            //Destroy(gameObject);
+        }
+
+        GetComponent<BoxCollider2D>().enabled = true;
+
+        transform.rotation = Quaternion.Euler(myVector);
     }
 }
