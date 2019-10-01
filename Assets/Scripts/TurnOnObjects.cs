cc0395da64f297d5dc1d48547a3797f9364ed30e
Thomas2124 <40778004+Thomas2124@users.noreply.github.com>
2019-10-01 15:32:52 +1000

    worked on menu

---
+++ Assets/Scripts/TurnOnObjects.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOnObjects : MonoBehaviour
+{
+    public GameObject[] objects;
+    // Start is called before the first frame update
+    void OnEvent()
+    {
+        foreach (GameObject item in objects)
+        {
+            item.SetActive(false);
+            item.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
