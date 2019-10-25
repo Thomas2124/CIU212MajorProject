9cb2c93516f078cf6fe1cf95034ed3e2fb1367ae
Thomas2124 <40778004+Thomas2124@users.noreply.github.com>
2019-10-25 16:33:32 +1100

    fixed dead light bug

--- Assets/Scripts/Thomas_Scripts/NewDeadLight.cs
+++ Assets/Scripts/Thomas_Scripts/NewDeadLight.cs
@@ -7,15 +7,17 @@ public class NewDeadLight : MonoBehaviour
     public List<GameObject> listObjects;
     public float radius = 6f;
     public GameObject[] lights;
+    public GameObject[] blockObjects;
 
 
-    void OnEnable()
+    void Start()
     {
+        blockObjects = GameObject.FindGameObjectsWithTag("Blocks");
         // Check if objects can be found.
-        if (GameObject.FindGameObjectsWithTag("DeadLight") != null)
-        {
-            lights = GameObject.FindGameObjectsWithTag("DeadLight");
-        }
+        //if (GameObject.FindGameObjectsWithTag("DeadLight") != null)
+        //{
+        //    lights = GameObject.FindGameObjectsWithTag("DeadLight");
+        //}
 
         // Get all collider within a radius.
         Collider2D[] hit = Physics2D.OverlapCircleAll(transform.position, radius);
@@ -26,56 +28,57 @@ public class NewDeadLight : MonoBehaviour
             GameObject currentObject = item.gameObject;
             SpriteRenderer currentrenderer = currentObject.GetComponent<SpriteRenderer>();
 
-            if (currentObject.tag != "FallingPlatform" && currentrenderer != null) 
+            if (currentObject.tag == "FallingPlatform" || currentrenderer == null)
             {
-                if (lights != null && lights.Length > 1) // If the array has more than two elements.
+                continue;
+            }
+
+            if (blockObjects.Length > 0)
+            {
+                // Cycles through all the objects and checks if an object is already in the same position.
+                for (int j = 0; j < blockObjects.Length; j++)
                 {
-                    for (int i = 0; i < lights.Length; i++)
+                    if (Vector2.Distance(gameObject.transform.position, blockObjects[j].transform.position) > radius * 2f)
                     {
-                        GameObject currentLight = lights[i];
+                        continue;
+                    }
+                    GameObject listedObject = blockObjects[j];
+                    SpriteRenderer listedRenderer = listedObject.GetComponent<SpriteRenderer>();
 
-                        if (currentLight != gameObject) // Checks if the current element doesn't equal this gameobject
-                        {
-                            // Cycles through all the objects and checks if an object is already in the same position.
-                            for (int j = 0; j < currentLight.GetComponent<NewDeadLight>().listObjects.Count; j++)
-                            {
-                                GameObject listedObject = currentLight.GetComponent<NewDeadLight>().listObjects[j];
-                                SpriteRenderer listedRenderer = listedObject.GetComponent<SpriteRenderer>();
+                    if (currentObject.transform.position == listedObject.transform.position) // resets the alpha value of the renderer.
+                    {
+                        listedObject.isStatic = false;
+                        listedRenderer.enabled = true;
+                        print("match");
+                        Color myColor = listedRenderer.material.color;
+                        Color myColor2 = currentrenderer.material.color;
+                        float newAlpha = myColor.a + myColor2.a;
 
-                                if (currentObject.transform.position == listedObject.transform.position) // resets the alpha value of the renderer.
-                                {
-                                    print("match");
-                                    Color myColor = listedRenderer.material.color;
-                                    Color myColor2 = currentrenderer.material.color;
-                                    float newAlpha = myColor.a + myColor2.a;
+                        if (newAlpha > 1f)
+                            newAlpha = 1f;
 
-                                    if (newAlpha > 1f)
-                                        newAlpha = 1f;
-
-                                    listedRenderer.material.color = new Color(myColor.r, myColor.g, myColor.b, newAlpha);
-                                    make = false;
-                                }
-                            }
-                        }
+                        listedRenderer.material.color = new Color(myColor.r, myColor.g, myColor.b, newAlpha);
+                        make = false;
                     }
                 }
-
-                if (make == true) // If there is no match, create an object and add it to a list.
-                {
-                    GameObject theObject = Instantiate(item.gameObject);
-                    theObject.isStatic = false;
-                    Vector3 objectPos = theObject.transform.position;
-                    BoxCollider2D objectCollider = theObject.GetComponent<BoxCollider2D>();
-                    SpriteRenderer objectRenderer = theObject.GetComponent<SpriteRenderer>();
-                    theObject.transform.position = new Vector3(objectPos.x, objectPos.y, objectPos.z);
-                    theObject.tag = "Blocks";
-                    objectCollider.enabled = false;
-                    objectRenderer.sortingLayerID = 0;
-                    objectRenderer.sortingOrder = 2;
-                    theObject.isStatic = true;
-                    listObjects.Add(theObject);
-                }
-            } 
+            }
+            
+            if(make == true)
+            {
+                GameObject theObject = Instantiate(item.gameObject);
+                theObject.isStatic = false;
+                Vector3 objectPos = theObject.transform.position;
+                BoxCollider2D objectCollider = theObject.GetComponent<BoxCollider2D>();
+                SpriteRenderer objectRenderer = theObject.GetComponent<SpriteRenderer>();
+                objectRenderer.enabled = true;
+                theObject.transform.position = new Vector3(objectPos.x, objectPos.y, objectPos.z);
+                theObject.tag = "Blocks";
+                objectCollider.enabled = false;
+                objectRenderer.sortingLayerID = 0;
+                objectRenderer.sortingOrder = 2;
+                //theObject.isStatic = true;
+                listObjects.Add(theObject);
+            }
         }
 
         // After checking all the objects in the Hit array, set the alpha values in the listObjects array based on distance.
