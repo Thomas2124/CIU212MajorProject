e359e2b1d2c251412d20d9b1780ef6463c0cc852
Thomas2124 <40778004+Thomas2124@users.noreply.github.com>
2019-11-08 18:48:09 +1100

    The game now forces the tutorial onto new players

---
+++ Assets/Scripts/Thomas_Scripts/FirstTime.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstTime : MonoBehaviour
+{
+    public GameObject falseStartButton; // Fake start button
+    public GameObject trueStartButton; // Real start button
+    public GameObject howToButton; // Real start button
+    public GameObject learnPanel;
+
+    public int checkNum;
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        checkNum = PlayerPrefs.GetInt("First");
+
+        if (checkNum == 0)
+        {
+            falseStartButton.SetActive(true);
+            trueStartButton.SetActive(false);
+            howToButton.SetActive(false);
+        }
+    }
+
+    public void MyFirst()
+    {
+        PlayerPrefs.SetInt("First", 1);
+        learnPanel.SetActive(true);
+    }
+}
